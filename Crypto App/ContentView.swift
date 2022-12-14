be41c79410df5df3f19aa84d437201d645396904
Sinead <sinead@Christophs-MacBook-Pro.local>
2022-12-14 15:46:16 +0100

    initial commit

---
+++ Crypto App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Crypto App
+//
+//  Created by Sinead on 14.12.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
